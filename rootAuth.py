281a2674a08e6cb65b3ca172d7f0bf777c2adf7d
geumseongYoon <ygs9405@naver.com>
2019-04-18 16:54:14 +0900

    basic form for root authentification

--- rootAuth.py
+++ rootAuth.py
@@ -6,6 +6,6 @@ if euid != 0:
     print("Script not started as root. Running sudo..")
     args = ['sudo', sys.executable] + sys.argv + [os.environ]
     # the next line replaces the currently-running process with the sudo
-    os.execlpe('sudo', *args)
+    # os.execlpe('sudo', *args)
 else:
     print('Running. Your euid is', euid)
