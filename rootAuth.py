5227d68961b8f43e296a47a2add8166e85aee986
geumseongYoon <ygs9405@naver.com>
2019-04-18 16:21:50 +0900

    first commit

---
+++ rootAuth.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+euid = os.geteuid()
+if euid != 0:
+    print("Script not started as root. Running sudo..")
+    args = ['sudo', sys.executable] + sys.argv + [os.environ]
+    # the next line replaces the currently-running process with the sudo
+    os.execlpe('sudo', *args)
+else:
+    print('Running. Your euid is', euid)
